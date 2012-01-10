17ee1aa6d736ce84af4500f0868a08b71d661732
Raymond Cox <rj.cox101@gmail.com>
2012-01-09 23:03:19 -0500

    Added hacky warping

--- com/turbonips/troglodytes/entities/Layer.java
+++ com/turbonips/troglodytes/entities/Layer.java
@@ -8,10 +8,19 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public abstract class Layer extends Entity {
 	
-	protected final TiledMap tiledMap;
+	protected TiledMap tiledMap;
 	protected Vector2f off;
 	private final int PLAYER_SPEED = 8;
-	private final int WALL_LAYER = 3;
+	private final int ATTRIBUTE_LAYER = 3;
+	
+	// Attributes
+	private final int NONE_ATT = 0;
+	private final int WALL_ATT = 1;
+	private final int WARP_ATT = 2;
+	
+	// UGLY HACK
+	private int groupID = 0;
+	private int objectID = 0;
 	
 	protected Vector2f slidingMin;
 	protected Vector2f slidingMax;
@@ -31,90 +40,158 @@ public abstract class Layer extends Entity {
 		TILE_SIZE=32;
 	}
 	
-	private boolean isBlocked(int tileX, int tileY) {
-		boolean blocked = false;
+	private int getAttribute(int tileX, int tileY) {
+		int attribute = NONE_ATT;
 		
 		if (tileX < 0  || tileX >= tiledMap.getWidth()) {
-			blocked = true;
+			attribute = WALL_ATT;
 		} else if (tileY < 0  || tileY >= tiledMap.getHeight()) {
-			blocked = true;
+			attribute = WALL_ATT;
 		} else {
-			int tileId = tiledMap.getTileId(tileX, tileY, WALL_LAYER);
-			if (tiledMap.getTileProperty(tileId, "Type", "None").equals("Wall")) {
-				blocked = true;
+			int tileId = tiledMap.getTileId(tileX, tileY, ATTRIBUTE_LAYER);
+			String attType = tiledMap.getTileProperty(tileId, "Type", "None").toLowerCase();
+			if (attType.equals("wall")) {
+				attribute = WALL_ATT;
+			} else {
+				for (int groupID=0; groupID<tiledMap.getObjectGroupCount(); groupID++) {
+					for (int objectID=0; objectID<tiledMap.getObjectCount(groupID); objectID++) {
+						int x = tiledMap.getObjectX(groupID, objectID)/TILE_SIZE;
+						int y = tiledMap.getObjectY(groupID, objectID)/TILE_SIZE;
+						attType = tiledMap.getObjectType(groupID, objectID).toLowerCase();
+						
+						if (x == tileX && y == tileY) {
+							if (attType.equals("warp")) {
+								attribute = WARP_ATT;
+								this.groupID = groupID;
+								this.objectID = objectID;
+							}
+						}
+					}
+				}
 			}
-			if (blocked) logger.info(tileX + "," + tileY + ": " + tileId);
 		}
-		
-		
-		return blocked;
+		return attribute;
+	}
+	
+	private int getAttribute(int x1, int y1, int x2, int y2) {
+		int att1 = getAttribute(x1,y1);
+		int att2 = getAttribute(x2,y2);
+		if (att1 == NONE_ATT) return att2;
+		else return att1;
+	}
+	
+	private void moveVertical(Input input) {
+		if(input.isKeyDown(Input.KEY_DOWN)) {
+			if (slidingPos.y >= slidingMax.y) {
+				off.y -= PLAYER_SPEED;
+			} else {
+				slidingPos.y += PLAYER_SPEED;
+			}
+			
+			playerLoc.y += PLAYER_SPEED;
+		} else if (input.isKeyDown(Input.KEY_UP)) {
+			if (slidingPos.y <= slidingMin.y) {
+				off.y += PLAYER_SPEED;
+			} else {
+				slidingPos.y -= PLAYER_SPEED;
+			}
+			playerLoc.y -= PLAYER_SPEED;
+		}
+	}
+	
+	private void moveHorizontal(Input input) {
+		if(input.isKeyDown(Input.KEY_RIGHT)) {
+			if (slidingPos.x >= slidingMax.x) {
+				off.x -= PLAYER_SPEED;
+			} else {
+				slidingPos.x += PLAYER_SPEED;
+			}
+
+			playerLoc.x += PLAYER_SPEED;
+		} else if (input.isKeyDown(Input.KEY_LEFT)) {
+			if (slidingPos.x <= slidingMin.x) {
+				off.x += PLAYER_SPEED;
+			} else {
+				slidingPos.x -= PLAYER_SPEED;
+			}
+			
+			playerLoc.x -= PLAYER_SPEED;
+		}
+	}
+	
+	private void warpPlayer(String warpMap, int warpX, int warpY) throws Exception {
+		this.tiledMap = new TiledMap("resources/" + warpMap);
+		this.playerLoc = new Vector2f(warpX*TILE_SIZE, warpY*TILE_SIZE);
+		this.slidingPos = new Vector2f(0, 0);
+		this.off = new Vector2f(warpX*-TILE_SIZE, warpY*-TILE_SIZE);
 	}
 	
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+		int attVertical = NONE_ATT;
+		int attHorizontal = NONE_ATT;
+		
+		
 		Input input = container.getInput();
-		if(input.isKeyDown(Input.KEY_DOWN)) { 
+		if(input.isKeyDown(Input.KEY_DOWN)) {
 			int leftBottomX, leftBottomY, rightBottomX, rightBottomY;
 			leftBottomX = (int)Math.floor((playerLoc.x)/TILE_SIZE);
 			leftBottomY = (int)Math.floor((playerLoc.y+playerSize.y-0.1f+PLAYER_SPEED)/TILE_SIZE);
 			rightBottomX = (int)Math.floor((playerLoc.x+playerSize.x-0.1f)/TILE_SIZE);
 			rightBottomY = (int)Math.floor((playerLoc.y+playerSize.y-0.1f+PLAYER_SPEED)/TILE_SIZE);
-			if (!(isBlocked(leftBottomX,leftBottomY) || isBlocked(rightBottomX,rightBottomY))) {
-				if (slidingPos.y >= slidingMax.y) {
-					off.y -= PLAYER_SPEED;
-				} else {
-					slidingPos.y += PLAYER_SPEED;
-				}
-				
-				playerLoc.y += PLAYER_SPEED;
-			}
+			attVertical = getAttribute(leftBottomX,leftBottomY,rightBottomX,rightBottomY);
+			
 		} else if (input.isKeyDown(Input.KEY_UP)) {
 			int leftTopX, leftTopY, rightTopX, rightTopY;
 			leftTopX = (int)Math.floor((playerLoc.x)/TILE_SIZE);
 			leftTopY = (int)Math.floor((playerLoc.y-PLAYER_SPEED)/TILE_SIZE);
 			rightTopX = (int)Math.floor((playerLoc.x+playerSize.x-0.1f)/TILE_SIZE);
 			rightTopY = (int)Math.floor((playerLoc.y-PLAYER_SPEED)/TILE_SIZE);
-			if (!(isBlocked(leftTopX,leftTopY) || isBlocked(rightTopX,rightTopY))) {
-				if (slidingPos.y <= slidingMin.y) {
-					off.y += PLAYER_SPEED;
-				} else {
-					slidingPos.y -= PLAYER_SPEED;
-				}
-				
-				playerLoc.y -= PLAYER_SPEED;
-			}
+			attVertical = getAttribute(leftTopX,leftTopY,rightTopX,rightTopY);
 		}
 		
-		if(input.isKeyDown(Input.KEY_RIGHT)) {
+	   if(input.isKeyDown(Input.KEY_RIGHT)) {
 			int topRightX, topRightY, bottomRightX, bottomRightY;
 			topRightX = (int)Math.floor((playerLoc.x+playerSize.x-0.1f+PLAYER_SPEED)/TILE_SIZE);
 			topRightY = (int)Math.floor((playerLoc.y)/TILE_SIZE);
 			bottomRightX = (int)Math.floor((playerLoc.x+playerSize.x-0.1f+PLAYER_SPEED)/TILE_SIZE);
 			bottomRightY = (int)Math.floor((playerLoc.y+playerSize.y-0.1f)/TILE_SIZE);
-			if (!(isBlocked(topRightX,topRightY) || isBlocked(bottomRightX,bottomRightY))) {
-				if (slidingPos.x >= slidingMax.x) {
-					off.x -= PLAYER_SPEED;
-				} else {
-					slidingPos.x += PLAYER_SPEED;
-				}
-	
-				playerLoc.x += PLAYER_SPEED;
-			}
+			attHorizontal = getAttribute(topRightX,topRightY,bottomRightX,bottomRightY);
+			
 		} else if (input.isKeyDown(Input.KEY_LEFT)) {
 			int topLeftX, topLeftY, bottomLeftX, bottomLeftY;
 			topLeftX = (int)Math.floor((playerLoc.x-PLAYER_SPEED)/TILE_SIZE);
 			topLeftY = (int)Math.floor((playerLoc.y)/TILE_SIZE);
 			bottomLeftX = (int)Math.floor((playerLoc.x-PLAYER_SPEED)/TILE_SIZE);
 			bottomLeftY = (int)Math.floor((playerLoc.y+playerSize.y-0.1f)/TILE_SIZE);
-			if (!(isBlocked(topLeftX,topLeftY) || isBlocked(bottomLeftX,bottomLeftY))) {
-				if (slidingPos.x <= slidingMin.x) {
-					off.x += PLAYER_SPEED;
-				} else {
-					slidingPos.x -= PLAYER_SPEED;
-				}
-				
-				playerLoc.x -= PLAYER_SPEED;
-			}
+			attHorizontal = getAttribute(topLeftX,topLeftY,bottomLeftX,bottomLeftY);
 		}
+	   
+	   if (attHorizontal != WALL_ATT) {
+		   moveHorizontal(input);
+	   }
+	   if (attVertical != WALL_ATT) {
+		   moveVertical(input);
+	   }
+	   
+	   int att = attVertical;
+	   if (attHorizontal != NONE_ATT) att = attHorizontal;
+	   
+	   switch (att) {
+		   case WARP_ATT:
+			   String warpMap = tiledMap.getObjectProperty(groupID, objectID, "Map", "none");
+			   int warpX = Integer.valueOf(tiledMap.getObjectProperty(groupID, objectID, "X", "0"));
+			   int warpY = Integer.valueOf(tiledMap.getObjectProperty(groupID, objectID, "Y", "0"));
+				try {
+					warpPlayer(warpMap, warpX, warpY);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					logger.fatal(e);
+				}
+
+			   break;
+	   }
+	   
 	}
 }
