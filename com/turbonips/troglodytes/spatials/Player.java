f7877fdc091c331c0a9e5107df2692cfd29ed604
Robert Sikorski <sikorski.rob@gmail.com>
2012-01-22 20:31:43 -0500

    Added changes to Player.java necessary for rendering animations.

--- com/turbonips/troglodytes/spatials/Player.java
+++ com/turbonips/troglodytes/spatials/Player.java
@@ -7,11 +7,13 @@ import org.newdawn.slick.Image;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.World;
+import com.turbonips.troglodytes.components.AnimationCreature;
 import com.turbonips.troglodytes.components.Sliding;
 
 public class Player extends Spatial {
 	private Sliding sliding;
 	private Image sprite;
+	private AnimationCreature animationCreature;
 	private final GameContainer container;
 
 	public Player(World world, Entity owner, Object form, GameContainer container) {
@@ -24,15 +26,31 @@ public class Player extends Spatial {
 	public void initalize() {
 		ComponentMapper<Sliding> slidingMapper = new ComponentMapper<Sliding>(Sliding.class, world);
 		sliding = slidingMapper.get(owner);
+		ComponentMapper<AnimationCreature> animationCreatureMapper = new ComponentMapper<AnimationCreature>(AnimationCreature.class, world);
+		animationCreature = animationCreatureMapper.get(owner);
 	}
 
 	@Override
 	public void render(Graphics g) {
 		//TODO: Make the commented out code work because it is more correct
 		if (sliding != null) {
-			g.drawImage(sprite, container.getWidth()/2 - sliding.getX(), container.getHeight()/2 - sliding.getY());
+			if (animationCreature != null)
+			{
+				g.drawAnimation(animationCreature.getCurrent(), container.getWidth()/2 - sliding.getX(), container.getHeight()/2 - sliding.getY());
+			}
+			else
+			{
+				g.drawImage(sprite, container.getWidth()/2 - sliding.getX(), container.getHeight()/2 - sliding.getY());
+			}
 		} else {
-			g.drawImage(sprite, container.getWidth()/2, container.getHeight()/2);
+			if (animationCreature != null)
+			{
+				g.drawAnimation(animationCreature.getCurrent(), container.getWidth()/2, container.getHeight()/2);
+			}
+			else
+			{
+				g.drawImage(sprite, container.getWidth()/2, container.getHeight()/2);
+			}
 		}
 	}
 
