46e058df550fa5bfc9f51d6492df22637c35b26e
Raymond Cox <rj.cox101@gmail.com>
2012-01-23 18:44:36 -0500

    Added debug text showing player tile position

--- com/turbonips/troglodytes/states/PlayingState.java
+++ com/turbonips/troglodytes/states/PlayingState.java
@@ -16,6 +16,7 @@ import com.turbonips.troglodytes.EntityFactory;
 import com.turbonips.troglodytes.components.SpatialForm;
 import com.turbonips.troglodytes.systems.CollisionSystem;
 import com.turbonips.troglodytes.systems.ControlSystem;
+import com.turbonips.troglodytes.systems.DebugTextSystem;
 import com.turbonips.troglodytes.systems.LightingSystem;
 import com.turbonips.troglodytes.systems.ObjectSystem;
 import com.turbonips.troglodytes.systems.RenderSystem;
@@ -30,6 +31,7 @@ public class PlayingState extends BaseGameState {
 	private EntitySystem collisionSystem;
 	private EntitySystem lightingSystem;
 	private EntitySystem objectSystem;
+	private EntitySystem debugTextSystem;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
@@ -42,6 +44,7 @@ public class PlayingState extends BaseGameState {
 		collisionSystem = systemManager.setSystem(new CollisionSystem(container));
 		lightingSystem = systemManager.setSystem(new LightingSystem(container));
 		objectSystem = systemManager.setSystem(new ObjectSystem(container));
+		debugTextSystem = systemManager.setSystem(new DebugTextSystem(container));
 		systemManager.initializeAll();
 		
 		EntityFactory.create(world, EntityFactory.ID_GROUND_LAYER);
@@ -56,6 +59,7 @@ public class PlayingState extends BaseGameState {
 			throws SlickException {
 		renderSystem.process();
 		lightingSystem.process();
+		debugTextSystem.process();
 	}
 
 	@Override
@@ -66,6 +70,7 @@ public class PlayingState extends BaseGameState {
 		controlSystem.process();
 		collisionSystem.process();
 		objectSystem.process();
+		
 	}
 
 	@Override
