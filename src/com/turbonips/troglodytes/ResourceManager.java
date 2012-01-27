cbc97a18a4313f4f66eed2bec4cdc14b76f8c64c
Raymond Cox <rj.cox101@gmail.com>
2012-01-27 11:33:58 -0500

    Added resource system

---
+++ src/com/turbonips/troglodytes/ResourceManager.java
@@ -0,0 +1,42 @@
+package com.turbonips.troglodytes;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+import org.newdawn.slick.SlickException;
+
+public class ResourceManager {
+	private static final ResourceManager instance = new ResourceManager();
+	private final Logger logger = Logger.getLogger(getClass());
+	private HashMap<String, Resource> resources = new HashMap<String, Resource>();
+	
+	private ResourceManager() {
+	}
+	
+	public static ResourceManager getInstance() {
+		return instance;
+	}
+	
+	public Resource getResource(String id) {
+		Resource resource = null;
+		ResourceFactory resourceFactory = ResourceFactory.getInstance();
+		
+		try {
+			resource = resources.get(id);
+			if (resource == null) {
+				// Create the resource
+				resource = resourceFactory.create(id);
+				resources.put(id, resource);
+			}
+			
+		} catch(SlickException ex) {
+			logger.error(ex);
+		}
+		return resource;
+	}
+	
+	public void unloadResource(String id) {
+		resources.put(id, null);
+	}
+
+}
