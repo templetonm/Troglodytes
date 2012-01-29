24672fb2d0a2d9bd004734ef0a6fa7c8cc575705
Robert Sikorski <sikorski.rob@gmail.com>
2012-01-29 16:35:31 -0500

    Added MusicSystem class so I could push the music branch.

---
+++ src/com/turbonips/troglodytes/systems/MusicSystem.java
@@ -0,0 +1,23 @@
+package com.turbonips.troglodytes.systems;
+
+import com.artemis.Entity;
+import com.artemis.utils.ImmutableBag;
+
+public class MusicSystem extends BaseEntitySystem
+{
+
+	@Override
+	protected boolean checkProcessing()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void processEntities(ImmutableBag<Entity> arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
