a8526733d6e8049f69b9d79b0dc3af97757ef184
Robert Sikorski <sikorski.rob@gmail.com>
2012-02-28 14:22:13 -0500

    Added empty SoundSystem class so I could push the branch.

---
+++ src/com/turbonips/troglodytes/systems/SoundSystem.java
@@ -0,0 +1,22 @@
+package com.turbonips.troglodytes.systems;
+
+import com.artemis.Entity;
+import com.artemis.utils.ImmutableBag;
+
+public class SoundSystem extends BaseEntitySystem
+{
+
+	@Override
+	protected boolean checkProcessing()
+	{
+		return true;
+	}
+
+	@Override
+	protected void processEntities(ImmutableBag<Entity> arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
