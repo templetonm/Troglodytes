0b194c2fc99691d127a145a1785a57da572e54bb
unknown <Ben@Ben-TOP.(none)>
2012-05-10 18:12:33 -0400

    New Map

--- src/com/turbonips/troglodytes/states/PlayingState.java
+++ src/com/turbonips/troglodytes/states/PlayingState.java
@@ -66,7 +66,7 @@ public class PlayingState extends BaseGameState {
 		// Setup the initial player
 		Entity player = world.createEntity();
 		player.setGroup("PLAYER");
-		player.addComponent(new Warp("trog1", new Vector2f(20,20)));
+		player.addComponent(new Warp("testing", new Vector2f(20,20)));
 		player.addComponent(new ResourceRef("playeranimation"));
 		//player.addComponent();
 		player.addComponent(new Movement(10, new Vector2f(2,2), new Vector2f(2,2)));
